ad8b1994e9ad92a9f6524962bec35e6bd1644627
Duong Minh Hoang <hoangdm.141196@gmail.com>
2023-07-26 01:01:45 +0700

    Added clever tag invalidations

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -22,7 +22,11 @@ const albumsApi = createApi({
     return {
       fetchAlbums: builder.query({
         providesTags: (result, error, user) => {
-          return [{ type: "Album", id: user.id }];
+          const tags = result.map((album) => {
+            return { type: "Album", id: album.id };
+          });
+          tags.push({ type: "UsersAlbums", id: user.id });
+          return tags;
         },
         query: (user) => {
           return {
@@ -36,7 +40,7 @@ const albumsApi = createApi({
       }),
       addAlbum: builder.mutation({
         invalidatesTags: (result, error, user) => {
-          return [{ type: "Album", id: user.id }];
+          return [{ type: "UsersAlbums", id: user.id }];
         },
         query: (user) => {
           return {
@@ -52,7 +56,7 @@ const albumsApi = createApi({
       removeAlbum: builder.mutation({
         invalidatesTags: (result, error, album) => {
           console.log(album);
-          return [{ type: "Album", id: album.userId }];
+          return [{ type: "Album", id: album.id }];
         },
         query: (album) => {
           return {
