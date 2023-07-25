c1a854329a04d1d62dfefd0ed6f4b3b04e7c0b6a
Duong Minh Hoang <hoangdm.141196@gmail.com>
2023-07-26 00:45:09 +0700

    Added removeAlbum mutation api

---
+++ src/components/AlbumsListItem.jsx
@@ -0,0 +1,34 @@
+import { GoTrash } from "react-icons/go";
+import Button from "./Button";
+import ExpandablePanel from "./ExpandablePanel";
+import { useRemoveAlbumMutation } from "../store";
+import { Fragment } from "react";
+
+function AlbumsListItem({ album }) {
+  const [removeAlbum, results] = useRemoveAlbumMutation();
+
+  const handleRemoveAlbum = () => {
+    removeAlbum(album);
+  };
+
+  const header = (
+    <Fragment>
+      <Button
+        className="mr-2"
+        loading={results.isLoading}
+        onClick={handleRemoveAlbum}
+      >
+        <GoTrash />
+      </Button>
+      {album.title}
+    </Fragment>
+  );
+
+  return (
+    <ExpandablePanel key={album.id} header={header}>
+      List of photos in the album
+    </ExpandablePanel>
+  );
+}
+
+export default AlbumsListItem;
