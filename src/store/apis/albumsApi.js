5f38be5c5e406cc71ab1a6872de200b7bdb4f8e2
Duong Minh Hoang <hoangdm.141196@gmail.com>
2023-07-26 00:50:02 +0700

    Easy Tag Invalidation

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -50,6 +50,10 @@ const albumsApi = createApi({
         },
       }),
       removeAlbum: builder.mutation({
+        invalidatesTags: (result, error, album) => {
+          console.log(album);
+          return [{ type: "Album", id: album.userId }];
+        },
         query: (album) => {
           return {
             url: `/albums/${album.id}`,
